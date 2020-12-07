3be76d88e27b07b1d16c6e9a7f661e6dd140e457
Armand Fardeau <fardeauarmand@gmail.com>
2020-12-07 21:30:13 +0100

    Add complete fixture response

--- src/spec/app_spec.cr
+++ src/spec/app_spec.cr
@@ -7,19 +7,18 @@ describe "App" do
         end
 
         it "returns default questions" do
-            api_response.body.should contain("q")
-            api_response.body.should contain("a")
+            api_response.body.should eq("{\"q\":\"Are you a robot ?\",\"a\":[\"68934a3e9455fa72420237eb05902327\",\"b326b5062b2f0e69046810717534cb09\"]}"
+)
         end
     end
 
     context "when visiting english route" do
         it "returns a success response" do
-            api_response("locale=en").status_code.should eq(200)
+            api_response("?locale=en").status_code.should eq(200)
         end
 
         it "returns a question" do
-            api_response("locale=en").body.should contain("q")
-            api_response("locale=en").body.should contain("a")
+            api_response("?locale=en").body.should eq("{\"q\":\"Are you a robot ?\",\"a\":[\"68934a3e9455fa72420237eb05902327\",\"b326b5062b2f0e69046810717534cb09\"]}")
         end
     end
 
@@ -29,19 +28,17 @@ describe "App" do
         end
 
         it "returns default questions" do
-            api_response("locale=fr").body.should contain("q")
-            api_response("locale=fr").body.should contain("a")
+            api_response("?locale=fr").body.should eq("{\"q\":\"Êtes-vous une machine ?\",\"a\":[\"14b8f0494c6f1460c3720d0ce692dbca\",\"b2a5abfeef9e36964281a31e17b57c97\"]}")
         end
     end
 
     context "when visiting a route that is not defined" do
         it "returns a success response" do
-            api_response("locale=ys").status_code.should eq(200)
+            api_response("?locale=ys").status_code.should eq(200)
         end
 
         it "returns default questions" do
-            api_response("locale=ys").body.should contain("q")
-            api_response("locale=ys").body.should contain("a")
+            api_response("?locale=ys").body.should eq("{\"q\":\"Are you a robot ?\",\"a\":[\"68934a3e9455fa72420237eb05902327\",\"b326b5062b2f0e69046810717534cb09\"]}")
         end
     end
 end
